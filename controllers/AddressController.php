3e5251f35c962e1e5e0a27d8a77ed15b30a41113
Todd Salpen <toddsalpen@gmail.com>
2025-05-07 22:22:16 -0400

    fix: address controller

--- controllers/AddressController.php
+++ controllers/AddressController.php
@@ -50,12 +50,19 @@ class AddressController extends BaseController {
             // Validate person exists
             // Make sure to strictly compare with ===
             if ((int)$data[Address::PERSON_TYPE] === PersonType::USER) {
-                $person = User::findFirst($data[Address::PERSON_ID]);
+                $person = User::findFirst([
+                    'conditions' => 'id = :id:',
+                    'bind' => ['id' => $data[Address::PERSON_ID]]
+                ]);
                 if (!$person) {
                     return $this->respondWithError(Message::USER_NOT_FOUND, 404);
                 }
-            } else {
-                $person = Patient::findFirst($data[Address::PERSON_ID]);
+            }
+            else {
+                $person = Patient::findFirst([
+                    'conditions' => 'id = :id:',
+                    'bind' => ['id' => $data[Address::PERSON_ID]]
+                ]);
                 if (!$person) {
                     return $this->respondWithError(Message::PATIENT_NOT_FOUND, 404);
                 }
