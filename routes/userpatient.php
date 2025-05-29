1ef53dfd15aea9a28967c6791530f4eec5968f69
Todd Salpen <toddsalpen@gmail.com>
2025-05-29 14:37:42 -0400

    hot fix lana

--- routes/userpatient.php
+++ routes/userpatient.php
@@ -5,12 +5,21 @@ if (isset($app)) {
     // User-patient assignment
     // JSON Body example { "user_id": 2, "patient_id": 12}
     $userPatient = new UserPatientController();
-    $app->post('/assign/patient', [$userPatient, 'create']);
+
+
+    $app->post('/user/assign/patient', [$userPatient, 'create']);
 
     // Get assigned patients by user ID
-    $app->get('/assigned/patients/{userId}', [$userPatient, 'getUserAssignedPatients']);
+    $app->get('/user/{userId}/patients', [$userPatient, 'getUserAssignedPatients']);
     // Get patients NOT assigned to user ID
-    $app->get('/unassigned/patients/{userId}', [$userPatient, 'getUserUnassignedPatients']);
+//    $app->get('/unassigned/patients/{userId}', [$userPatient, 'getUserUnassignedPatients']);
+
+//    $app->post('/assign/patient', [$userPatient, 'create']);
+//
+//    // Get assigned patients by user ID
+//    $app->get('/assigned/patients/{userId}', [$userPatient, 'getUserAssignedPatients']);
+//    // Get patients NOT assigned to user ID
+//    $app->get('/unassigned/patients/{userId}', [$userPatient, 'getUserUnassignedPatients']);
 
 
     // Get assigned users by patient ID
