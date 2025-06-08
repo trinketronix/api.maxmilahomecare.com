65d7abdc53b2d26b7ab1363c8d22f9da863d1be3
Todd Salpen <toddsalpen@gmail.com>
2025-06-08 00:23:25 -0400

    save user id

--- routes/user.php
+++ routes/user.php
@@ -4,7 +4,7 @@ use Api\Controllers\UserController;
 
 if (isset($app)) {
     $user = new UserController();
-    $app->put('/user', [$user, 'updateUser']);
+    $app->put('/user/{userid}', [$user, 'updateUser']);
     $app->put('/user/update/photo', [$user, 'updatePhoto']);
     $app->post('/user/upload/photo', [$user, 'uploadPhoto']);
 }
