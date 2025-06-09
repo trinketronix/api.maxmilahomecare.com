5e21872d3081dbea0710b792e76b83a149417269
Todd Salpen <toddsalpen@gmail.com>
2025-06-09 12:55:30 -0400

    feature: get single photo

--- routes/user.php
+++ routes/user.php
@@ -5,6 +5,7 @@ use Api\Controllers\UserController;
 if (isset($app)) {
     $user = new UserController();
     $app->put('/user/{userId:[0-9]*}', [$user, 'updateUser']);
+    $app->get('/user/photo/{userId:[0-9]*}', [$user, 'getPhoto']);
     $app->put('/user/update/photo', [$user, 'updatePhoto']);
     $app->post('/user/upload/photo', [$user, 'uploadPhoto']);
 }
