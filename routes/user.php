d138f6e698dccd7a3d08f4212b7f0e39d3afb2fe
Todd Salpen <toddsalpen@gmail.com>
2025-06-11 23:56:59 -0400

    implementing: upload and update photo for boss roles like administrator and manager

--- routes/user.php
+++ routes/user.php
@@ -4,8 +4,24 @@ use Api\Controllers\UserController;
 
 if (isset($app)) {
     $user = new UserController();
+
+    // Update user data
     $app->put('/user/{userId:[0-9]*}', [$user, 'updateUser']);
+
+    // Get user photo
     $app->get('/user/photo/{userId:[0-9]*}', [$user, 'getPhoto']);
-    $app->put('/user/update/photo', [$user, 'updatePhoto']);
+
+    // Upload photo routes
+    // For uploading own photo (backward compatibility)
     $app->post('/user/upload/photo', [$user, 'uploadPhoto']);
+
+    // For uploading photo for a specific user (admin/manager)
+    $app->post('/user/{userId:[0-9]+}/upload/photo', [$user, 'uploadPhoto']);
+
+    // Update photo routes
+    // For updating own photo (backward compatibility)
+    $app->put('/user/update/photo', [$user, 'updatePhoto']);
+
+    // For updating photo for a specific user (admin/manager)
+    $app->put('/user/{userId:[0-9]+}/update/photo', [$user, 'updatePhoto']);
 }
